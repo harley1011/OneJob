6c361e9c58d408f20a2ae8ad9a93fd591b3645d4
harley1011 <harley.1011@gmail.com>
2016-01-23 11:06:31 -0500

    Added gulp modules to package file

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,15 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var sourcemaps = require('gulp-sourcemaps')
+var uglify = require('gulp-uglify')
+var ngAnnotate = require('gulp-ng-annotate')
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/*.scss', './scss/partials/*'],
+  controllers: ['./www/js/controllers/*.js'],
+  services: ['./www/js/services/*.js'],
+  directives: ['./www/js/directives/*.js']
 };
 
 gulp.task('default', ['sass']);
@@ -23,6 +29,39 @@ gulp.task('sass', function(done) {
     }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('controllers', function (done) {
+  gulp.src(['./www/js/controllerModule.js', paths.controllers[0]])
+    .pipe(sourcemaps.init())
+    .pipe(concat('controllers.js'))
+    .pipe(ngAnnotate())
+    // .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./www/js'))
+    .on('end', done);
+});
+
+gulp.task('services', function (done) {
+  gulp.src(['./www/js/serviceModule.js', paths.services[0]])
+    .pipe(sourcemaps.init())
+    .pipe(concat('services.js'))
+    .pipe(ngAnnotate())
+    // .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./www/js'))
+    .on('end', done);
+});
+
+gulp.task('directives', function (done) {
+  gulp.src(['./www/js/directiveModule.js', paths.directives[0]])
+    .pipe(sourcemaps.init())
+    .pipe(concat('directives.js'))
+    .pipe(ngAnnotate())
+    // .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./www/js'))
     .on('end', done);
 });
 

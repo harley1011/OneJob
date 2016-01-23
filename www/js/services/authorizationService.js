9e57cd4396a3d39fe12e576fb7a644c459db6825
A-Hubbell <austin.hubbell@gmail.com>
2016-01-23 13:33:39 -0500

    Added new services

---
+++ www/js/services/authorizationService.js
@@ -0,0 +1,15 @@
+Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
+
+angular.module('services', []).service (
+    'authorizationService', function (userName, password) {
+        Parse.User.logIn(userName, password, {
+            success: function(user) {
+                console.log("success");
+                return true;
+            },
+            error: function (user, error) {
+                console.log(error);
+                return false;
+            }
+        });
+    });
---
+++ www/js/services/registrationService.js
@@ -0,0 +1,23 @@
+Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
+
+angular.module('services', []).service (
+    'registrationService', function (userName, email, firstName, lastName, password) {
+        var user = new Parse.User();
+        user.set("email", email);
+        user.set("firstName", firstName);
+        user.set("lastName", lastName);
+         user.set("password", password);
+        user.set("username", userName);
+
+
+        user.signUp(null, {
+            success: function (user) {
+                console.log("signup success");
+            },
+            error: function (user, error){
+                console.log("signup failed");
+                console.log (error);
+            }
+
+        });
+    });

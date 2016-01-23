7a8ba61261f95d7b61500484fd136fa982e4213e
harley1011 <harley.1011@gmail.com>
2016-01-23 14:18:41 -0500

    Adding callback to service

--- www/js/services/registrationService.js
+++ www/js/services/registrationService.js
@@ -2,7 +2,38 @@ angular.module('services')
   .service('registrationService', function () {
     Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
 
-    this.signUp = function (userName, email, firstName, lastName, password) {
+    var registerUser = {
+      email: "",
+      username: "",
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      password: ""
+    };
+
+    this.setNames = function (firstName, lastName)
+    {
+      registerUser.firstName = firstName;
+      registerUser.lastName = lastName;
+
+    }
+
+    this.setUsername = function (username)
+    {
+      registerUser.username = username;
+    }
+
+    this.setEmail = function (email)
+    {
+      registerUser.email = email;
+    }
+
+    this.getUser = function()
+    {
+      return registerUser;
+    }
+
+    this.signUp = function (userName, email, firstName, lastName, password, callback) {
       var user = new Parse.User();
       user.set("email", email);
       user.set("firstName", firstName);
@@ -14,10 +45,12 @@ angular.module('services')
       user.signUp(null, {
         success: function (user) {
           console.log("signup success");
+          callback({success: true, user :user});
         },
         error: function (user, error) {
           console.log("signup failed");
           console.log(error);
+          callback({success: false, user :user});
         }
 
       });

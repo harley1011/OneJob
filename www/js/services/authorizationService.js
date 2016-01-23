e7ed5512085b9823934cafe6733cd49dd7029c85
harley1011 <harley.1011@gmail.com>
2016-01-23 13:41:42 -0500

    Removing brackets from services

--- www/js/services/authorizationService.js
+++ www/js/services/authorizationService.js
@@ -1,6 +1,4 @@
-
-
-angular.module('services', []).service (
+angular.module('services').service (
     'authorizationService', function (userName, password) {
     Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
         Parse.User.logIn(userName, password, {
--- www/js/services/registrationService.js
+++ www/js/services/registrationService.js
@@ -1,4 +1,4 @@
-angular.module('services', []).service (
+angular.module('services').service (
     'registrationService', function (userName, email, firstName, lastName, password) {
     Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
 

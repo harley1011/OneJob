44edbbacd930e8bfe7486685b2b0ff2f9bdeccab
Hilary Chan <marshbox@Hilarys-MBP.wireless.concordia.ca>
2016-01-24 09:00:08 -0500

    added browse

--- www/js/controllers/feedCtrl.js
+++ www/js/controllers/feedCtrl.js
@@ -1,6 +1,16 @@
 angular.module('controllers')
 
-  .controller('feedCtrl', function ($scope) {
+  .controller('feedCtrl', function ($scope, $state, $ionicSlideBoxDelegate, jobService, $rootScope, tempStorageService) {
+    
+     $scope.init = function () {
+
+      jobService.returnAllJobs(50, null, null, function (result) {
+        if (result.jobs)
+          $scope.jobsPost = JSON.parse(JSON.stringify(result.jobs));
+        else
+          $scope.jobsPost = [];
+      })
+     };
 
   })
 

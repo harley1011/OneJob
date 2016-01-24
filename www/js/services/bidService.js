d3ade2059241ec898b269bf5835b51790942513f
harley1011 <harley.1011@gmail.com>
2016-01-24 04:08:56 -0500

    Stuff

--- www/js/services/bidService.js
+++ www/js/services/bidService.js
@@ -1,40 +1,61 @@
 angular.module('services').service(
   'bidService', function () {
     Parse.initialize("WNMS58WrCeFRP5GDL43J9EPtPaJuMUd7AsygsGlH", "B3M0Urq3fJtw7BoeW0zztFK1uA243PpdugTyfKMK");
+
+
     this.makeBid = function makeBid(jobID, bidValue, callback) {
       var Bid = Parse.Object.extend("Bid");
-      var bid = new Bid();
-      var user = Parse.User.current();
       var Job = Parse.Object.extend("Job");
 
+      var query = new Parse.Query(Bid);
+      var job = new Job();
+      job.id = jobID;
+      query.equalTo("contractor", Parse.User.current());
+      query.equalTo("job", job);
 
-      var query = new Parse.Query(Job);
-      query.equalTo("objectId", jobID);
       query.first({
-        success: function (results) {
-          bid.set("contractor", user);
-          bid.set("bidValue", bidValue);
-          bid.set("job", results);
-
-          bid.save(null, {
-            success: function () {
-              console.log("success");
-              callback({success: true, data: bid});
-              return true;
+        success: function(bid){
+          console.log(bid);
+          if (bid)
+          {
+            bid.set("bidValue", bidValue);
+          }
+          else
+          {
+            bid = new Bid();
+            bid.set("contractor", Parse.User.current());
+            bid.set("job", job);
+            bid.set("bidValue", bidValue);
+          }
+          bid.save({
+            success: function (result){
+              callback({success: true, bid: JSON.parse(JSON.stringify(result))})
             },
-            error: function () {
-              console.log("fail");
-              callback({success: false, message: error});
-              return false;
+            error: function(error){
+              console.log(error);
             }
-          });
+          })
+
+
         },
-        error: function (error) {
-          console.log(error);
+        error: function (error){
+
         }
-      });
+      })
+     /* bid.set("bidValue", bidValue);
 
-
+      bid.save(null, {
+        success: function () {
+          console.log("success");
+          callback({success: true, data: bid});
+          return true;
+        },
+        error: function () {
+          console.log("fail");
+          callback({success: false, message: error});
+          return false;
+        }
+      });*/
     }
 
     this.getBidHistory = function (limit, jobID, callback) {
